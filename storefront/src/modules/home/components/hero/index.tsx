e07d10251485b63b63b74108366ad274851166cf
BenGripzz <benbleiker08@gmail.com>
2023-08-27 15:31:05 +0200

    first commit

---
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -0,0 +1,33 @@
+import UnderlineLink from "@modules/common/components/underline-link"
+import Image from "next/image"
+
+const Hero = () => {
+  return (
+    <div className="h-[90vh] w-full relative">
+      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+        <h1 className="text-Summer2xl-semi mb-4 drop-shadow-md shadow-black">
+           Welcome to Gripzz
+        </h1>
+   
+        
+        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+          Uprade your Ride ASAP!
+        </p>
+        <UnderlineLink href="/store"> See Griptape</UnderlineLink>
+      </div>
+      <Image
+        src="/hero.jpg"
+        layout="fill"
+        loading="eager"
+        priority={true}
+        quality={90}
+        objectFit="cover"
+        alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
+        className="absolute inset-0"
+        draggable="false"
+      />
+    </div>
+  )
+}
+
+export default Hero
